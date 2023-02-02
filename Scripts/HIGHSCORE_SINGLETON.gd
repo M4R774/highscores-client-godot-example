f8ade7f924038171f9796757800e6d15962a4998
Aleksi Martikainen <aleksi.martikainen@devecto.com>
2023-02-02 14:38:56 +0200

    Add basic auth

--- Scripts/HIGHSCORE_SINGLETON.gd
+++ Scripts/HIGHSCORE_SINGLETON.gd
@@ -5,20 +5,26 @@ var SCORE = null
 
 var GAME_NAME : String = ""
 var BACKEND_DOMAIN : String = ""  # "example.com"
+var BACKEND_USERNAME : String = ""
+var BACKEND_PASSWORD : String = ""
 
 
 func _ready():
 	GAME_NAME = ProjectSettings.get_setting('application/config/name')
-	BACKEND_DOMAIN = get_domain_from_config_file()
+	set_values_from_config_file()
 	assert(GAME_NAME != "")
 	assert(BACKEND_DOMAIN != "")
+	assert(BACKEND_USERNAME != "")
+	assert(BACKEND_PASSWORD != "")
 
 
-func get_domain_from_config_file():
+func set_values_from_config_file():
 	var file = File.new()
 	if not file.file_exists("res://config.json"):
 		print("No config.json file found...")
 	file.open("res://config.json", File.READ)
 	var content_as_text = file.get_as_text()
 	var content_as_dictionary = parse_json(content_as_text)
-	return content_as_dictionary["domain"]
+	BACKEND_DOMAIN = content_as_dictionary["domain"]
+	BACKEND_USERNAME = content_as_dictionary["username"]
+	BACKEND_PASSWORD = content_as_dictionary["password"]
