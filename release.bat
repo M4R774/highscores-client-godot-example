a9658ea3369c929426c7f57454036993578f1306
Aleksi Martikainen <aleksi.martikainen@devecto.com>
2023-02-03 12:26:14 +0200

    Make script for one button releases

--- release.bat
+++
@@ -1,7 +0,0 @@
-godot --version
-godot --export html5
-
-Rem -a is for .zip file
-Rem -c is for creating an archive
-Rem -f is for archive filename
-tar.exe -caf html5_executable.zip Builds/*
---
+++ release_html5.bat
@@ -0,0 +1,14 @@
+godot --version
+godot --export HTML5
+
+Rem -a is for .zip file
+Rem -c is for creating an archive
+Rem -f is for archive filename
+Rem -C my_directory . makes that the contents are directly at the root of the
+Rem      archive, not inside Builds folder
+cd Builds/HTML5 
+tar.exe -caf ../html5_executable.zip *
+cd ..
+butler push html5_executable.zip martta/online-highscore-demo:html5
+
+pause
