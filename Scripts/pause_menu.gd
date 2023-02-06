61b8eb76b5e6ce548713233231627f9f33b6802f
Aleksi Martikainen <aleksi.martikainen@devecto.com>
2023-02-06 17:34:02 +0200

    Add missing import files and script

---
+++ Scripts/pause_menu.gd
@@ -0,0 +1,12 @@
+extends PanelContainer
+
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_cancel"):
+		self.visible = not self.visible
+		get_tree().paused = self.visible
+
+
+func _on_continue_pressed():
+	self.visible = false
+	get_tree().paused = self.visible
